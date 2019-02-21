5e226439489a9346907ebf8c3444f7b2ed2cedd4
Elias Keller <elias.keller@gmail.com>
2019-02-21 23:28:26 +0100

    feat: email function works!

--- EmailSend/views.py
+++ EmailSend/views.py
@@ -4,20 +4,8 @@ from django.core.mail import send_mail
 
 
 def index(request):
+    send_mail('Test-Mail', 'Hello there...;)',
+              'elias.keller@gmail.com',
+              ['rixuzixova@mail-hub.info'],
+              fail_silently=False)
     return render(request, 'tent/index.html')
-
-
-def send_email(request):
-    subject = request.POST.get('subject', '')
-    message = request.POST.get('message', '')
-    from_email = request.POST.get('from_email', '')
-    if subject and message and from_email:
-        try:
-            send_mail(subject, message, from_email, ['admin@example.com'])
-        except BadHeaderError:
-            return HttpResponse('Invalid header found.')
-        return HttpResponseRedirect('/contact/thanks/')
-    else:
-        # In reality we'd use a form class
-        # to get proper validation errors.
-        return HttpResponse('Make sure all fields are entered and valid.')
